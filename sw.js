34aa064c0557d67f422226eae1d91c4cff565835
agidseven-lang <agidseven@gmail.com>
2026-05-19 19:04:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Agenda ID Seven - service worker
    Estratégia: rede primeiro (sempre busca a versão mais nova quando online),
    com cache de reserva para funcionar offline. */
-var CACHE = "idseven-v41";
+var CACHE = "idseven-v45";
 
 self.addEventListener("install", function (e) {
   self.skipWaiting();
